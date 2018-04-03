67f656bdec0db7197d0b51413cf603f421b4181c
eduardosmatheus <matheus-edu@outlook.com>
2018-04-02 21:30:12 -0300

    Partial commit; Create category resources

--- app.py
+++ app.py
@@ -6,13 +6,26 @@ from db_configuration import Base, Category, CategoryItem
 app = Flask(__name__)
 
 engine = create_engine('sqlite:///catalog.db')
-
+Base.metadata.bind = engine
 Base.metadata.create_all(engine)
 
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    return "Hello world"
+@app.route('/categories')
+def get_categories():
+    categories = session.query(Category).all()
+    output = ""
+    for category in categories:
+        output += "{id} - {name} <br/>".format(id=category.category_id, name=category.category_name)
+    
+    return output
+
+@app.route('/categories')
+@app.route('/categories/<category_id>')
+def get_category():
+    category = session.query(Category)
 
 if __name__ == '__main':
     app.debug = True
