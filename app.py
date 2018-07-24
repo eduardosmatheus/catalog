19f1c1925193d75232b24f3d9a23b142ca9bfd5a
eduardosmatheus <matheus-edu@outlook.com>
2018-07-23 22:02:19 -0300

    Refactor to match pep8online requirements

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-#!/usr/bin/python3
-#encoding: utf-8
-from flask import Flask, render_template, request, redirect,jsonify, url_for, flash, make_response
+# !/usr/bin/python3
+# encoding: utf-8
+from flask import Flask, render_template, request
+from flask import redirect, jsonify, url_for, flash, make_response
 from flask import session as login_session
 from sqlalchemy import create_engine, update
 from sqlalchemy.orm import sessionmaker, joinedload
 from oauth2client.client import flow_from_clientsecrets, FlowExchangeError
 from db_configuration import Base, Category, CategoryItem, User
-import httplib2, random, string, json, requests
+import httplib2
+import random
+import string
+import json
+import requests
 
 app = Flask(__name__)
 app.secret_key = b'\x98z\xe5\xbb\xce\xba\x7f\x7f\x1bi?\x90\x96X7+'
@@ -21,15 +26,20 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 # Token de acesso à API de login.
-CLIENT_ID = json.loads(open('client-secrets.json', 'r').read())['web']['client_id']
+clientsecrets_file = open('client-secrets.json', 'r').read()
+CLIENT_ID = json.loads(clientsecrets_file)['web']['client_id']
+
 
 # Home page
 @app.route("/", methods=["GET"])
 def home():
-    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+    state = ''.join(random.choice(
+        string.ascii_uppercase + string.digits
+    ) for x in range(32))
     login_session['state'] = state
     data = session.query(Category).all()
     return render_template("categories.html", categories=data, state=state)
+
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
@@ -38,7 +48,7 @@ def gconnect():
         response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-        
+
     # Obtain authorization code
     code = request.data
 
@@ -55,7 +65,8 @@ def gconnect():
 
     # Check that the access token is valid.
     access_token = credentials.access_token
-    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+    url_query = ('?access_token=%s' % access_token)
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo%s' % url_query)
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
     # If there was an error in the access token info, abort.
@@ -92,7 +103,7 @@ def gconnect():
 
     # Get user info
     userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
-    params = { 'access_token': credentials.access_token, 'alt': 'json' }
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
     answer = requests.get(userinfo_url, params=params)
 
     data = answer.json()
@@ -109,23 +120,30 @@ def gconnect():
     login_session['user_id'] = user_id
     return redirect(url_for('getCategories'))
 
+
 @app.route('/gdisconnect')
 def gdisconnect():
     access_token = login_session.get('access_token')
     if access_token is None:
-        response = make_response(json.dumps('Current user not connected.'), 401)
+        response = make_response(
+            json.dumps('Current user not connected.'), 401
+        )
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    url = 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token
+    url_query = '?access_token=%s' % access_token
+    url = 'https://www.googleapis.com/oauth2/v1/tokeninfo%s' % url_query
     validateToken = httplib2.Http()
     result = json.loads(validateToken.request(url, 'GET')[1])
-    
+
     if result.get('error') is None:
-        url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
+        url_query = '?token=%s' % access_token
+        url = 'https://accounts.google.com/o/oauth2/revoke%s' % url_query
         h = httplib2.Http()
-        result = h.request(url, 'GET', headers={'content-type': 'application/x-www-form-urlencoded'})[0]
-        
+        result = h.request(url, 'GET', headers={
+            'content-type': 'application/x-www-form-urlencoded'
+        })[0]
+
         if result['status'] == '200':
             del login_session['access_token']
             del login_session['gplus_id']
@@ -135,7 +153,9 @@ def gdisconnect():
             del login_session['user_id']
             return redirect(url_for('home'))
         else:
-            response = make_response(json.dumps('Failed to revoke token for given user.'), 400)
+            response = make_response(
+                json.dumps('Failed to revoke token for given user.'), 400
+            )
             response.headers['Content-Type'] = 'application/json'
             return response
     else:
@@ -147,8 +167,13 @@ def gdisconnect():
         del login_session['user_id']
         return redirect(url_for('home'))
 
+
 def createUser(login_session):
-    newUser = User(name=login_session['username'], email=login_session['email'], picture=login_session['picture'])
+    newUser = User(
+        name=login_session['username'],
+        email=login_session['email'],
+        picture=login_session['picture']
+    )
     session.add(newUser)
     session.commit()
     user = session.query(User).filter_by(email=login_session['email']).one()
@@ -167,31 +192,38 @@ def getUserID(email):
     except:
         return None
 
+
 @app.route('/categories')
 def getCategories():
     categories = session.query(Category).all()
     return render_template("categories.html", categories=categories)
 
+
 @app.route('/categories/<int:id>')
 def getCategory(id):
     category = session.query(Category).get(id)
-    items = session.query(CategoryItem).filter(CategoryItem.category_id == id).all()
+    items = session.query(CategoryItem).filter(
+        CategoryItem.category_id == id
+    ).all()
     return render_template("category.html", category=category, items=items)
+
 
 @app.route("/categories/new", methods=["GET"])
 def newCategory():
     return render_template("category_form.html", category=None)
 
+
 @app.route("/categories/new", methods=["POST"])
 def addNewCategory():
     try:
         currentUser = getUserID(login_session['email'])
-        newCategory = Category(name = request.form["name"], user_id=currentUser)
+        newCategory = Category(name=request.form["name"], user_id=currentUser)
         session.add(newCategory)
         session.commit()
     except:
         session.rollback()
     return redirect(url_for("home"))
+
 
 @app.route("/categories/<int:id>/edit", methods=["GET", "POST"])
 def updateCategory(id):
@@ -205,7 +237,8 @@ def updateCategory(id):
         return redirect(url_for("home"))
     else:
         return render_template("category_form.html", category=currentCategory)
-    
+
+
 @app.route("/categories/<int:id>/delete")
 def deleteCategory(id):
     currentCategory = session.query(Category).get(id)
@@ -213,16 +246,18 @@ def deleteCategory(id):
     session.commit()
     return redirect(url_for("home"))
 
+
 @app.route("/categories/<int:id>/items/new")
 def newCategoryItem(id):
-    return render_template("newcategoryitem.html", id = id)
+    return render_template("newcategoryitem.html", id=id)
+
 
 @app.route('/categories/<int:id>/items', methods=["POST"])
 def addNewCategoryItem(id):
     currentUser = getUserID(login_session['email'])
     newCategoryItem = CategoryItem(
-        name = request.form["name"],
-        details = request.form["details"],
+        name=request.form["name"],
+        details=request.form["details"],
         category_id=id,
         user_id=currentUser
     )
@@ -233,15 +268,18 @@ def addNewCategoryItem(id):
         session.rollback()
     return redirect(url_for("home"))
 
+
 @app.route("/categories/<int:id>/items/<int:item_id>")
 def getCategoryItem(id, item_id):
     item = session.query(CategoryItem).get(item_id)
-    return render_template("category_item.html", item = item)
+    return render_template("category_item.html", item=item)
+
 
 @app.route("/categories/<int:id>/items/<int:item_id>/edit", methods=["GET"])
 def editCategoryItem(id, item_id):
     currentItem = session.query(CategoryItem).get(item_id)
     return render_template("editcategoryitem.html", item=currentItem)
+
 
 @app.route("/categories/<int:id>/items/<int:item_id>/edit", methods=["POST"])
 def updateCategoryItem(id, item_id):
@@ -254,12 +292,14 @@ def updateCategoryItem(id, item_id):
         session.rollback()
     return redirect(url_for("home"))
 
+
 @app.route("/categories/<int:id>/items/<int:item_id>/delete")
 def deleteCategoryItem(id, item_id):
     currentItem = session.query(CategoryItem).get(item_id)
     session.delete(currentItem)
     session.commit()
     return redirect(url_for("home"))
+
 
 if __name__ == '__main__':
     app.config['SESSION_TYPE'] = 'filesystem'
